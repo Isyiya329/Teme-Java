6289cfde76cc48235fac81ad7f5eeeee20163b59
Stolojanu Maria-Denisa <denisa17stolojanu@gmail.com>
2025-01-17 17:05:04 +0200

    Update Vers.java

--- Laborator2/src/Problema2/Vers.java
+++ Laborator2/src/Problema2/Vers.java
@@ -4,12 +4,10 @@ public class Vers
 {
     private String text;
 
-    // Constructor
     public Vers(String text) {
         this.text = text;
     }
 
-    // Metodă pentru a număra cuvintele din vers
     public int numarCuvinte() {
         if (text.trim().isEmpty()) {
             return 0;
@@ -17,7 +15,6 @@ public class Vers
         return text.trim().split("\\s+").length;
     }
 
-    // Metodă pentru a număra vocalele din vers
     public int numarVocale() {
         int count = 0;
         String lowerCaseText = text.toLowerCase();
@@ -29,17 +26,14 @@ public class Vers
         return count;
     }
 
-    // Metodă pentru a verifica dacă versul se termină cu o grupare specificată
     public boolean seTerminaCu(String grupare) {
         return text.endsWith(grupare);
     }
 
-    // Metodă pentru a converti textul în majuscule
     public void toUpperCase() {
         this.text = this.text.toUpperCase();
     }
 
-    // Metodă pentru a obține textul versului
     public String getText() {
         return text;
     }
