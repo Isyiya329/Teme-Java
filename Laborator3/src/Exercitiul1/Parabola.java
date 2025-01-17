0a07316ed7b0e677f244a4f1c6b802620946305c
Stolojanu Maria-Denisa <denisa17stolojanu@gmail.com>
2025-01-17 17:20:51 +0200

    Add files via upload

---
+++ Laborator3/src/Exercitiul1/Parabola.java
@@ -0,0 +1,37 @@
+package Exercitiul1;
+
+public class Parabola
+{
+    private int a, b, c;
+
+    public Parabola(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double[] getVarf() {
+        double x = -b / (2.0 * a);
+        double y = -((b * b) - 4 * a * c) / (4.0 * a);
+        return new double[]{x, y};
+    }
+
+    @Override
+    public String toString() {
+        return "f(x) = " + a + "x^2 + " + b + "x + " + c;
+    }
+
+    public static double[] calculeazaMijloc(Parabola p1, Parabola p2) {
+        double[] varf1 = p1.getVarf();
+        double[] varf2 = p2.getVarf();
+        double x = (varf1[0] + varf2[0]) / 2;
+        double y = (varf1[1] + varf2[1]) / 2;
+        return new double[]{x, y};
+    }
+
+    public static double calculeazaLungime(Parabola p1, Parabola p2) {
+        double[] varf1 = p1.getVarf();
+        double[] varf2 = p2.getVarf();
+        return Math.hypot(varf2[0] - varf1[0], varf2[1] - varf1[1]);
+    }
+}
