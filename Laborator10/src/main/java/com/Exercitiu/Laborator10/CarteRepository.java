eca67dc34ea85f900961eec48a41f4986953b607
Stolojanu Maria-Denisa <denisa17stolojanu@gmail.com>
2025-01-17 17:14:55 +0200

    Delete Laborator10 directory

--- Laborator10/src/main/java/com/Exercitiu/Laborator10/CarteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Exercitiu.Laborator10;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-interface CarteRepository extends JpaRepository<Carte, String> {
-    List<Carte> findByAutor(String autor);
-}
